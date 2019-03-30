b51e7e05af500d666c3c3e702211b08ff54ef660
Daniel <danielmercado@cvsc.com.co>
2019-03-30 11:06:58 -0500

    *Modulo pedidos:   -modificacion en la vista de aprobacion de prefacturas para que solo se mostrasen las prefacturas creadas desde el 2019-03-29.

--- controllers/PedidoController.php
+++ controllers/PedidoController.php
@@ -3763,7 +3763,7 @@ class PedidoController extends Controller
         $query = (new \yii\db\Query())
         ->select('id,dependencia,ceco,cebe,marca,regional,empresa,mes,ano,total_fijo,total_variable,total_mes,ciudad')
         ->from('prefactura_consolidado_pedido')
-        ->where('estado_pedido="S" AND estado="cerrado"');
+        ->where('estado_pedido="S" AND estado="cerrado" AND DATE(created) >= "2019-03-29"');
         //FILTROS
         if(isset($_GET['enviar'])){
            
