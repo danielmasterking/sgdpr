42a029104e8ff8f9097b5cc1ccdc0a1909267130
Daniel Mercado <daniel.mercado@zalvadora.com>
2019-09-28 20:20:29 -0500

    *archivo para configurar db.

---
+++ config/_db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=host_name;dbname=database_name',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
+];
