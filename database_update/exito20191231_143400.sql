e645d6f9dce317b291b58914452060a4658d3b91
Daniel Mercado <daniel.mercado@zalvadora.com>
2020-01-02 15:34:47 -0500

    actualizacion bd

---
+++ database_update/exito20191231_143400.sql
@@ -0,0 +1,9 @@
+INSERT INTO `detalle_servicio` (`servicio_id`, `codigo`, `descripcion`, `precio`, `precio_nocturno`, `ano`) VALUES
+(1, 'ASA', 'Sin Arma', '2349371.000', '3080305.000', '2019'),
+(1, 'ACA', 'Con Arma', '2392878.000', '3137348.000', '2019'),
+(1, 'ACD', 'Can Defensa ', '2414631.000', '3165869.000', '2019'),
+(1, 'ACE', 'Can Explosivo', '2414631.000', '3165869.000', '2019'),
+(2, 'PSA', 'Sin Arma', '2349371.000', '3080305.000', '2019'),
+(2, 'PCA', 'Con Arma', '2392878.000', '3137348.000', '2019'),
+(2, 'PCD', 'Can Defensa ', '2414631.000', '3165869.000', '2019'),
+(2, 'PCE', 'Can Explosivo', '2414631.000', '3165869.000', '2019');
